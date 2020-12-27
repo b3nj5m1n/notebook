6720b16f153b1a3bcf1bb3b74851625438fa01ed
b3nj4m1n <b3nj4m1n@gmx.net>
2020-12-27 05:30:23 +0100

    Added Advent of Code python template to writeups

---
+++ writeups/adventofcode/2020/template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import fileinput
+import re
+
+# Read in the file input
+inp = [int(line.strip()) for line in fileinput.input()]
+# Variables for storing the results
+part_1 = None
+part_2 = None
+
+# Code
+[print(line) for line in inp]
+
+# Print the results
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
