725d7c1b70025d0368eef263a6a92b5128b6fba6
b3nj4m1n <b3nj4m1n@gmx.net>
2020-12-28 16:33:33 +0100

    Fixed Advent of Code ruby template in writeups

--- writeups/adventofcode/2020/template.rb
+++ writeups/adventofcode/2020/template.rb
@@ -4,12 +4,12 @@ require 'set'
 # Read in the file input
 inp = File.open(ARGV[0]).readlines
 # Variables for storing the results
-Part_1 = nil
-Part_2 = nil
+part_1 = nil
+part_2 = nil
 
 # Code
 inp.each { |line| puts line }
 
 # Print the results
-puts "Part 1: #{Part_1}"
-puts "Part 2: #{Part_2}"
+puts "Part 1: #{part_1}"
+puts "Part 2: #{part_2}"
